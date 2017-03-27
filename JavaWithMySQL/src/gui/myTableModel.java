3d3054c85161bf819ff2346fc7ea1487fcbeffdb
Cyxap <cyxap1987@gmail.com>
2017-03-27 22:28:10 +0300

    TableModel см.

--- JavaWithMySQL/src/gui/myTableModel.java
+++ JavaWithMySQL/src/gui/myTableModel.java
@@ -4,12 +4,15 @@ package gui;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
-public class myTableModel implements TableModel{
+public class myTableModel implements TableModel {
+
+    private final String [] tableHeaders = {"Number ige", "Depth monolit", "Work load 0.05", "Work load 0.10",
+            "Work load 0.15", "Work load 0.20","Work load 0.25", "Work load 0.30",
+            "Starting pressure", "Mochnost pros sloia", "Density SG", "Density real",
+            "Density 0.8", "Litology pressure", "Otnosit prossadka", "Prosadka"};
 
     @Override
-    public int getRowCount() {
-        return 0;
-    }
+    public int getRowCount() {return 0;}
 
     @Override
     public int getColumnCount() {
@@ -18,11 +21,53 @@ public class myTableModel implements TableModel{
 
     @Override
     public String getColumnName(int columnIndex) {
-        return null;
+        switch (columnIndex){
+            case 0:
+                return tableHeaders[columnIndex];
+            case 1:
+                return tableHeaders[columnIndex];
+            case 2:
+                return tableHeaders[columnIndex];
+            case 3:
+                return tableHeaders[columnIndex];
+            case 4:
+                return tableHeaders[columnIndex];
+            case 5:
+                return tableHeaders[columnIndex];
+            case 6:
+                return tableHeaders[columnIndex];
+            case 7:
+                return tableHeaders[columnIndex];
+            case 8:
+                return tableHeaders[columnIndex];
+            case 9:
+                return tableHeaders[columnIndex];
+            case 10:
+                return tableHeaders[columnIndex];
+            case 11:
+                return tableHeaders[columnIndex];
+            case 12:
+                return tableHeaders[columnIndex];
+            case 13:
+                return tableHeaders[columnIndex];
+            case 14:
+                return tableHeaders[columnIndex];
+            case 15:
+                return tableHeaders[columnIndex];
+        }
+        return "";
     }
 
     @Override
     public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return Integer.class;
+            case 1:
+                return Double.class;
+            case 2:
+                return Double.class;
+        }
         return null;
     }
 
